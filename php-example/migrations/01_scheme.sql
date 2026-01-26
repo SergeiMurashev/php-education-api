0b37e0607c258a39ecc68e8f31b48390af04ee17
murashev07a24sv <sssrrr15v@gmail.com>
2026-01-26 16:52:23 +0500

    Добавляет таблицу comment

--- php-example/migrations/01_scheme.sql
+++ php-example/migrations/01_scheme.sql
@@ -41,3 +41,16 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
 
 CREATE INDEX IF NOT EXISTS idx_refresh_tokens_user_id ON refresh_tokens(user_id);
 CREATE INDEX IF NOT EXISTS idx_refresh_tokens_expires_at ON refresh_tokens(expires_at);
+
+CREATE TABLE comment (
+    id UUID PRIMARY KEY DEFAULT get_random_uuid(),
+    post_id UUID NOT NULL REFERENCES posts(id),
+    user_id UUID NOT NULL REFERENCES users(id),
+    content TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ NULL
+)
+
+CREATE INDEX IF NOT EXISTS indx_comment_post_id ON comment(post_id);
+CREATE INDEX IF NOT EXISTS indx_comment_user_id ON comment(user_id);
